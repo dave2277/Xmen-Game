046435be7e3dede8d096607f9c71dc82133488f9
Dave Gray <davegray@Daves-MacBook-Air.local>
2017-05-30 21:34:49 -0600

    resolved attack function issues

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -27,7 +27,6 @@ var cyclops = {
     counterPower: 13
 }
 
-
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
@@ -40,23 +39,24 @@ $( document ).ready(function() {
         }
         if (this.id === "storm") {
            var attacker = storm;
-        }
-        if (this.id === "wolverine") {
-            var attacker = wolverine;
-        }
-        if (this.id === "cyclops") {
-            var attacker = cyclops;
-        }
-        var enemy = $(".character").not(this)
-        $(enemy).appendTo( $("#opponents") );
-        $(enemy).removeClass("character");
-        $(enemy).addClass("adversary");
-        $("#choose").hide();
-        if ($(enemy).hasClass("adversary")) {
-            $(enemy).off();
-        }
+       }
+       if (this.id === "wolverine") {
+        var attacker = wolverine;
+    }
+    if (this.id === "cyclops") {
+        var attacker = cyclops;
+    }
 
-        $(".adversary").on("click", function() {
+    var enemy = $(".character").not(this)
+    $(enemy).appendTo( $("#opponents") );
+    $(enemy).removeClass("character");
+    $(enemy).addClass("adversary");
+    $("#choose").hide();
+    if ($(enemy).hasClass("adversary")) {
+        $(enemy).off();
+    }
+
+    $(".adversary").on("click", function() {
         $(this).appendTo( $(".defender") );
         if (this.id === "gambit") {
             var defender = gambit;
@@ -67,52 +67,56 @@ $( document ).ready(function() {
         }
 
         if (this.id === "wolverine") {
-            var defender = wolverine;
+        var defender = wolverine;
         }
 
         if (this.id === "cyclops") {
-            var defender = cyclops;
+        var defender = cyclops;
         }
 
-        $("#attack").on("click", function() {
+    $("#attack").on("click", function() {
         var counter = $('#attack').val();
         counter++;
-        $('#attack').val(counter);    
-        var cumulativeDamage = (function ()  
-        {
-            return function () {
-                    return (defender.healthPoints - (damage * (counter - 1)))
-                }
-            if (attacker.healthPoints <= 0) {
-                alert(over);
-            }
-
-            if  (defender.healthPoints <= 0) {
-                alert(win);
-            }
-
-        })();
-
-
-         
         
-
+        var damage = (attacker.attackPower * counter);
+        var attackerDamage = (attacker.healthPoints - (defender.counterPower * counter));
         var damage = (attacker.attackPower * counter);
 
-        var attackerDamage = (attacker.healthPoints - (defender.counterPower * counter));
+        $('#attack').val(counter);    
+        var cumulativeDamage = function () {
+            let result = (defender.healthPoints - (damage * counter))
+            defender.healthPoints = result;
+            attacker.healthPoints = attackerDamage;
+            return result;    
+        };
+        var newDamage = cumulativeDamage();
+
+        var over = "<h1> GAME OVER! </h1>" +
+        "<p>Your character has been defeated.</p>";
+        "<p>Press reset to play again. </p>";
+
 
         var status = "<p>" + attacker.name + " attacks for " + damage + " damage." +
-        "<p>" + defender.name + " has " + cumulativeDamage() + " HP. </p>" +
+        "<p>" + defender.name + " has " + newDamage + " HP. </p>" +
         "<p>" + defender.name + " counter attacks for " + defender.counterPower + " damage." +
         "<p>" + attacker.name + " has " + attackerDamage + " HP. </p>";
 
-        var over = "<h1> GAME OVER! </h1>" +
-        "<p>Press reset to play again </p>";
-
         var win = "<p> " + defender.name + " has been defeated! </p>" +
         "<p> Choose your next opponent. "
-        
-        });
-        });
+
+        $("#display").html(status); 
+
+        if (defender.healthPoints <= 0) {
+            $("#display").html(win);
+            $(".defender").empty();
+
+        }
+        console.log(attacker.healthPoints);
+        if (attacker.healthPoints <= 0) {
+            $("#display").html(over);
+
+        }
     });
+});
+});
 });
